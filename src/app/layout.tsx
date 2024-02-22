57f5a25eb481bda52b412bc525ab7a8af643258d
Paulo Mendes <paulo.h.mendes25@gmail.com>
2024-02-22 17:16:03 -0300

    feat: adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Montserrat, Rubik } from 'next/font/google';
 import './globals.css';
 import { Header } from '@/components/Header';
+import { Analytics } from '@vercel/analytics/react';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -29,6 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
