26ffd8dfd1e343f860c0d492873605d64fb6ed14
Paulo Mendes <paulo.h.mendes25@gmail.com>
2024-02-26 08:37:05 -0300

    fix: fixing text and values

--- src/layout/SeeHowStatisticsWork/index.tsx
+++ src/layout/SeeHowStatisticsWork/index.tsx
@@ -13,20 +13,16 @@ export const SeeHowStatisticsWork = () => {
       </div>
       <div className="flex flex-col gap-[37px] md:gap-12 max-w-[518px] w-full px-[48px] pb-[69px] md:px-0 md:pb-0">
         <h2 className="font-rubik text-[28px] leading-[34px] md:text-[40px] md:leading-[44px]">
-          Tome <b className="font-extrabold">decisões de apostas</b> embasadas em{' '}
-          <b className="font-extrabold">análise de classe mundial.</b>
+          Desconfiado das estatísticas? <b className="font-extrabold">Veja como elas funcionam.</b>
         </h2>
         <p className="text-lg md:text-[22px] leading-[28px]">
-          Nosso sistema de análise processa uma{' '}
-          <b className="font-bold">grande quantidade de dados</b>, incluindo histórico de jogos,
-          performance de jogadores e tendências de mercado.
+          Entendemos isso! Mas na <b>Betstats</b>, nós quebramos esse paradigma, fornecendo dados
+          transparentes e precisos.
           <br />
           <br />
-          Este método garante que cada recomendação seja baseada em análises técnicas e precisas,
-          permitindo que você faça{' '}
-          <b className="font-bold">
-            apostas com confiança, guiadas por um conhecimento profundo e insights valiosos.
-          </b>
+          Nossas análises são fundamentadas, verificáveis e comprovadamente eficazes, permitindo que
+          você tome decisões de apostas baseadas em informações reais, não em suposições. Nosso
+          sistema de análise processa uma{' '}
         </p>
       </div>
     </section>
