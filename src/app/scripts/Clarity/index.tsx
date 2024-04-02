7426958776a1ed16f0d7960e6d483d5e53996749
Paulo Mendes <paulo.h.mendes25@gmail.com>
2024-04-02 10:45:19 -0300

    redeploy

--- src/app/scripts/Clarity/index.tsx
+++ src/app/scripts/Clarity/index.tsx
@@ -8,13 +8,11 @@ export const ClarityScript = () => {
       id="microsoft-clarity-init"
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
-        __html: `
-                (function(c,l,a,r,i,t,y){
+        __html: `(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "lqbg8km4np");
-                `,
+        })(window, document, "clarity", "script", "lqbg8km4np");`,
       }}
     />
   );
